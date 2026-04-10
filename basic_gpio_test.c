f0bc707020955f0359c7af788e1cb9714da3f444
Seth Lee <sethlee2024@gmail.com>
2026-04-10 12:37:55 -0400

    blackpill update

--- basic_gpio_test.c
+++ basic_gpio_test.c
@@ -5,19 +5,23 @@
 void EXTI0_IRQHandler(void) {
     if (EXTI->PR & (1 << 0)) {
         EXTI->PR |= (1 << 0);        // clear pending flag
-        GPIOA->ODR |= (1 << 5);      // turn on LED (PA5)
+        GPIOC->ODR ^= (1 << 13);   // active low — clear to turn ON
     }
 }
 
 int main(void) {
     // 1. Enable clocks
     RCC->AHB1ENR |= (1 << 0);        // GPIOA port clock enable
+    RCC->AHB1ENR |= (1 << 2);
     RCC->APB2ENR |= (1 << 14);       // SYSCFG clock (needed for EXTI routing)
 
-    // 2. PA0 as input & PA5 (LED) as output
+    // 2. PA0 as input & PA13 (LED) as output
     GPIOA->MODER &= ~(3 << 0);      // sets PA0 to input
-    GPIOA->MODER &= ~(3 << 10); // PA5 clear bits
-    GPIOA->MODER |= (1 << 10); // 01 = output
+    GPIOC->MODER &= ~(3 << 26); // PC13 clear bits
+    GPIOC->MODER |= (1 << 26); // 01 = output
+
+	GPIOC->ODR |= (1 << 13);   // LED OFF
+
 
     // 3. Route PA0 to EXTI0 via SYSCFG
     SYSCFG->EXTICR[0] &= ~(0xF);     // bits [3:0] = 0000 = GPIOA
@@ -33,7 +37,9 @@ int main(void) {
     // 6. Enable in NVIC
     NVIC_EnableIRQ(EXTI0_IRQn);  // EXTI13 shares handler with lines 10-15
 
+
     while(1){
+
     }
 }
 
